35afe46df946dbd2cf780fec3f46f99d9c02f701
uknowhuan <uknow-huan@qq.com>
2016-08-26 20:21:34 +0800

    a small project

---
+++ 数据帧显示/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(343, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 393);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "包信息";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column14,
+            this.Column19,
+            this.Column16,
+            this.Column15,
+            this.Column17,
+            this.Column18});
+            this.dataGridView2.Location = new System.Drawing.Point(6, 207);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(554, 180);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dispaly_RichTextBox);
+            // 
+            // Column14
+            // 
+            this.Column14.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column14.HeaderText = "同步头";
+            this.Column14.Name = "Column14";
+            this.Column14.ReadOnly = true;
+            // 
+            // Column19
+            // 
+            this.Column19.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column19.HeaderText = "单元计数器";
+            this.Column19.Name = "Column19";
+            this.Column19.ReadOnly = true;
+            // 
+            // Column16
+            // 
+            this.Column16.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column16.HeaderText = "版本号";
+            this.Column16.Name = "Column16";
+            this.Column16.ReadOnly = true;
+            // 
+            // Column15
+            // 
+            this.Column15.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column15.HeaderText = "474B4859";
+            this.Column15.Name = "Column15";
+            this.Column15.ReadOnly = true;
+            // 
+            // Column17
+            // 
+            this.Column17.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column17.HeaderText = "航天器标识符";
+            this.Column17.Name = "Column17";
+            this.Column17.ReadOnly = true;
+            // 
+            // Column18
+            // 
+            this.Column18.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column18.HeaderText = "虚拟信道标识符（HEX）";
+            this.Column18.Name = "Column18";
+            this.Column18.ReadOnly = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 181);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dispaly_RichTextBox);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(2, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 393);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "信息明细";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(124, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "捕获数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(323, 234);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.SelectData);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(124, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "清空数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "0";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "1";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "2";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "3";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "4";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "5";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "6";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "7";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "8";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "9";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "10";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "11";
+            this.Column12.Name = "Column12";
+            this.Column12.ReadOnly = true;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "12";
+            this.Column13.Name = "Column13";
+            this.Column13.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 397);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+    }
+}
+
---
+++ 数据帧显示/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        string[] path = { "data1.txt", "data2.txt", "data3.txt", "data4.txt" };   //声明数据源
+        byte[][] b = new byte[4][] { new byte[886], new byte[886], new byte[886], new byte[886] };
+        string[][] strarray1 = new string[4][] { new string[886], new string[886], new string[886], new string[886] };
+        public Form1()
+        {
+            InitializeComponent();
+            //设置列头不可选
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            foreach (DataGridViewColumn column in dataGridView2.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+
+        //设置“捕获数据”按钮的点击事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream[] afile = new FileStream[4];
+            for (int i = 0; i < 4; i++)
+            {
+                afile[i] = new FileStream(path[i], FileMode.Open);  //打开文件
+                afile[i].Read(b[i], 0, b[i].Length);                //将文件存入交叉数组b中
+                afile[i].Close();                                   //关闭流文件，释放资源
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                //将数据按16进制存入交叉数组staraary1中
+                for (int j = 0; j < 886; j++)
+                {
+                    strarray1[i][j] = b[i][j].ToString("X2");
+                }
+                //将数据按字节存入datagridview1中      
+                int index1 = dataGridView1.Rows.Add();
+                for (int j = 0; j < 13; j++)
+                {
+                    dataGridView1.Rows[index1].Cells[j].Value = strarray1[index1][j];
+                }
+            }
+
+            string[] datastr = new string[4];
+            string[][] strarray2 = new string[4][] { new string[6], new string[6], new string[6], new string[6] };
+            for (int i = 0; i < 4; i++)
+            {
+                //根据数据帧结构将某一区间数据转化成二进制
+                datastr[i] = Convert.ToString(b[i][8], 2).PadLeft(8, '0') + Convert.ToString(b[i][9], 2).PadLeft(8, '0');
+                //初始化交叉数组staarray2用于存储按帧结构划分的数据
+                strarray2[i][0] = strarray1[i][0] + strarray1[i][1] + strarray1[i][2] + strarray1[i][3];
+                strarray2[i][1] = strarray1[i][4] + strarray1[i][5] + strarray1[i][6] + strarray1[i][7];
+                strarray2[i][2] = datastr[i].Substring(0, 2);
+                strarray2[i][3] = datastr[i].Substring(2, 8);
+                strarray2[i][4] = datastr[i].Substring(10, 6);
+                strarray2[i][5] = strarray1[i][10] + strarray1[i][11] + strarray1[i][12];
+            }
+
+            //将数据按帧结构存入datagridview2中
+            for (int i = 0; i < 4; i++)
+            {
+                int index2 = dataGridView2.Rows.Add();
+                for (int j = 0; j < 6; j++)
+                {
+                    dataGridView2.Rows[index2].Cells[j].Value = strarray2[index2][j];
+                }
+            }
+            button1.Enabled = false;
+        }
+        //自定义函数GetOutput得出richtextbox1中的output
+        private string GetOutput(int RowIndex)
+        {
+            string output = "";
+            int j = 0;
+            switch (RowIndex)
+            {
+                case 0:
+                    for (int i = 0; i < 886; i++)
+                    {
+                        j = j + 1;
+                        if (j % 16 == 0)
+                            output += strarray1[0][i] + "\n";
+                        else
+                            output += strarray1[0][i] + " ";
+                    }
+                    break;
+                case 1:
+                    for (int i = 0; i < 886; i++)
+                    {
+                        j = j + 1;
+                        if (j % 16 == 0)
+                            output += strarray1[1][i] + "\n";
+                        else
+                            output += strarray1[1][i] + " ";
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 886; i++)
+                    {
+                        j = j + 1;
+                        if (j % 16 == 0)
+                            output += strarray1[2][i] + "\n";
+                        else
+                            output += strarray1[2][i] + " ";
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 886; i++)
+                    {
+                        j = j + 1;
+                        if (j % 16 == 0)
+                            output += strarray1[3][i] + "\n";
+                        else
+                            output += strarray1[3][i] + " ";
+                    }
+                    break;
+            }
+            return output;
+        }
+
+        //自定义函数将字符串转化为int32输出
+        private int converttoint(string s)
+        {
+            int i = Convert.ToInt32(s);
+            return i;
+        }
+
+        //设置CellClick事件将数据输入到richtextbox1里并用红色字体显示错误信息
+        private void Dispaly_RichTextBox(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex;
+            if (i >= 0)
+            {
+                richTextBox1.Text = "Data Hex_Output: (Length = 886 Bytes)" + "\n";
+                richTextBox1.Text = richTextBox1.Text + GetOutput(i);
+                //检测表格中中同步头是否出错
+                string line = richTextBox1.Lines[1];
+                string[] strarray = line.Split(' ');
+                string s1 = strarray[0] + strarray[1] + strarray[2] + strarray[3];
+                if (s1 != "1ACFFC1D")
+                {
+                    richTextBox1.Select(38, 11);
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+                //检测数据帧是否不连续
+                string s2 = (string)dataGridView2.Rows[i].Cells[5].Value;
+                int x = converttoint(s2);
+                //判断表格第一行中对应的计数器的计数值，不为0则用红色字体标出
+                if (i == 0)
+                {
+                    if (x != 0)
+                    {
+                        richTextBox1.Select(68, 8);
+                        richTextBox1.SelectionColor = Color.Red;
+                    }
+                }
+                //判断行之间计数器的计数值是否为连续整数，不是则用红色字体标出
+                else if (i > 0)
+                {
+                    string s3 = (string)dataGridView2.Rows[i - 1].Cells[5].Value;
+                    int y = converttoint(s3);
+                    if (x != y + 1)
+                    {
+                        richTextBox1.Select(68, 8);
+                        richTextBox1.SelectionColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        //设置“清空列表”按钮的点击事件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView2.Rows.Clear();
+            richTextBox1.Text = string.Empty;
+
+        }
+
+        //设置MouseClick事件用于点击richtextbox即可在表中显示位置所示处的数据内容
+        private void SelectData(object sender, MouseEventArgs e)
+        {
+            //清除两表格中所有cell的选定状态
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+            //获取光标的索引值
+            int i = richTextBox1.SelectionStart;
+            //设置光标位于数据帧的前13个字节内容时，点击事件才发生
+            if (i > 38 && i <= 76)
+            {
+                richTextBox1.Select(38, i - 38);                //选中数据头至光标位置的数据
+                string starter = richTextBox1.SelectedText;
+                string median = starter.Replace(" ", "");       //去掉starter中的空格,得到选中文本的连续字符串形式               
+                int j = median.Length;
+                //为了同步两个datagridview的行显示，设置条件为俩表格“当前行”索引值相等
+                if (dataGridView1.CurrentRow.Index == dataGridView2.CurrentRow.Index)
+                {
+                    int equal = dataGridView1.CurrentRow.Index;
+                    //将richtextbox中选中内容显示在datagridview1中
+                    if (j % 2 == 0)
+                    {
+                        for (int k = 0; k < j / 2; k++)
+                        {
+                            dataGridView1.Rows[equal].Cells[k].Selected = true;
+                            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                    if (j % 2 != 0)
+                    {
+                        for (int k = 0; k < (j + 1) / 2; k++)
+                        {
+                            dataGridView1.Rows[equal].Cells[k].Selected = true;
+                            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                    //同步将richtextbox中选中内容显示在datagridview2中
+                    if (j <= 8)
+                    {
+                        dataGridView2.Rows[equal].Cells[0].Selected = true;
+                        dataGridView2.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                    }
+                    if (j > 8 && j <= 16)
+                    {
+                        for (int k = 0; k < 2; k++)
+                        {
+                            dataGridView2.Rows[equal].Cells[k].Selected = true;
+                            dataGridView2.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                    if (j > 16 && j <= 18)
+                    {
+                        for (int k = 0; k < 4; k++)
+                        {
+                            dataGridView2.Rows[equal].Cells[k].Selected = true;
+                            dataGridView2.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                    if (j > 18 && j <= 20)
+                    {
+                        for (int k = 0; k < 5; k++)
+                        {
+                            dataGridView2.Rows[equal].Cells[k].Selected = true;
+                            dataGridView2.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                    if (j > 20 && j <= 26)
+                    {
+                        for (int k = 0; k < 6; k++)
+                        {
+                            dataGridView2.Rows[equal].Cells[k].Selected = true;
+                            dataGridView2.DefaultCellStyle.SelectionBackColor = Color.Aqua;
+                        }
+                    }
+                }
+                //当两表格当前行索引值不相等时，提示用户选相同行
+                else
+                {
+                    MessageBox.Show("请点击两表格的相同行(如先点击了datagridview1的第一行，应再点击下datagridview2的第一行，或先点击了datagridview2的第一行，则应再点击下datagridview1的第一行)", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+    }
+}
